587915d8d989e525e995fa3d7eed961165658986
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2019-11-17 23:57:21 +0400

    start commit

---
+++ app/src/main/java/com/rodionov/giphy_app/utils/Settings.kt
@@ -0,0 +1,11 @@
+package com.rodionov.giphy_app.utils
+
+/**
+ * Created by rodionov on 17.11.2019.
+ */
+class Settings {
+    companion object{
+        const val API_KEY = "hdPiOWIGpfGKYweD6qsLVxykgG7gL3Va"
+        const val BASE_URL = "http://api.giphy.com"
+    }
+}
